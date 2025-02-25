602726133b70b903093122a7d6f0f1bd0339f674
Andrea Burda <andrea.burda@ulb.be>
2025-02-25 14:54:09 +0100

    Added the first full version of level 1

--- meta-learning_main_script.py
+++ meta-learning_main_script.py
@@ -1,90 +1,87 @@
 #import ts and datasets
+import pandas as pd
 import tensorflow as tf
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt 
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
 
-###TO DO LIST
-#LVL 1
-##create the tasks: AND, XOR, RM (random mapping)
-##generalise the model structure to all tasks
-##export final data in a ending summary
-##compare performances with each other (statistics and plot)
-##Level 1 is done
+#TO DO LIST
+##LVL 1
+###export final data in a ending summary
+###compare performances with each other (statistics and plot)
+###Level 1 is done
 
-###CODE STARTS HERE
-#initialise
-learning_rate = 0.5
-epochs = 100
-
-#define the data set 
-##AND function
+#CODE STARTS HERE
+##DATA SET
 train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 test_x = np.copy(train_x)
-train_y = np.array([0, 0, 0, 1])
-train_y = train_y.reshape(4, 1)
 
-##XOR function
-'''train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-test_x = np.copy(train_x)
-train_y = np.array([0, 1, 1, 0])
-train_y = train_y.reshape(4, 1)'''
 
-##Randomised function
-'''to be done'''
-
-#input/output shape
+##MODEL STRUCTURE
 n_input, n_output = train_x.shape[1], 1 
 
-#learning model structure
-model = tf.keras.models.Sequential([
-    tf.keras.Input(shape=(n_input,)),
-    tf.keras.layers.Dense(n_output, activation="sigmoid")
-    ])
-model.build() 
+model = Sequential()
+model.add(Dense(
+    units=8, activation='tanh', input_shape=(n_input,)))
+model.add(Dense(
+    n_output, activation='sigmoid'))
+model.build()
 
-#train & test the model
-opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
-model.compile(optimizer = opt, loss=tf.keras.losses.MeanSquaredError())
-history = model.fit(train_x, train_y, batch_size = 1, epochs = epochs)
+model.compile(
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.1), 
+    loss = tf.keras.losses.BinaryCrossentropy(),
+    metrics = ['accuracy'])
+
+
+##Y_DATA SET + FIT
+epochs = 150
+
+###AND function
+'''model_name = 'AND_model'
+train_y = np.array([0, 0, 0, 1])
+train_y = train_y.reshape(4, 1)
+history = model.fit(train_x, train_y, batch_size = 1, epochs=epochs)
+loss_history = history.history["loss"]'''
+
+###XOR function
+model_name = 'XOR_model'
+train_y = np.array([0, 1, 1, 0])
+train_y = train_y.reshape(4, 1)
+history = model.fit(train_x, train_y, batch_size = 1, epochs=epochs)
+loss_history = history.history["loss"]
+
+###RM loop
+'''model_name = 'RM_model'
+loss_history = []
+accuracy_history = []
+
+for epoch in range(epochs):
+    train_y = np.random.randint(0, 2, size=(4, 1))
+    print(f"Epoch {epoch+1}/{epochs}")
+    history = model.fit(train_x, train_y, batch_size = 1, epochs=1, verbose=1)
+    loss_history.append(history.history["loss"][0]) 
+    accuracy_history.append(history.history["accuracy"][0])'''
+
+
+##GATHER DATA
+###store data
+test_results = model.evaluate(test_x, train_y)
+test_predictions = model.predict(test_x)
 model.summary()
 
-test_data = model.predict(test_x)
-
-#report data
+###report data: summary
 print(model.get_weights())
-fig, ax = plt.subplots(1)
-ax.plot(history.history["loss"], color = "black")
-
-#test data
 print("predictions on the test data:")
-print(test_data)
+print(test_predictions)
+print(f"Model name: {model_name}, Loss: {test_results[0]:.4f}, Accuracy: {test_results[1]:.4f}")
 
-#save the model
+###report data: plots
+fig, ax = plt.subplots(1)
+ax.plot(loss_history, color = "black")
+
+###save the model
 '''model.save("D:/ULB/MA2/STAGE2/code/internship_curriculum_model")'''
-
-
-
-###ADDITIONAL CODE FOR MY OWN USAGE
-'''def f(x): 
-    return (x - 1)**2
-
-def df(x):
-    return 2 * (x-1)
-
-x = 5 
-learning_rate = 0.1
-tolerance = 1e-6
-
-history = [x]
-
-for i in range(100):
-    gradient = df(x)
-    x = x - learning_rate * gradient
-    history.append(x)
-    if abs(gradient) < tolerance: 
-        break
-
-print(f"Minimum found at x = {x}, f(x) = {f(x)}")'''
 
 
