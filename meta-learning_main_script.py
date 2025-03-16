3fb474718ad73be158d5b77e7dac33bd153a209a
Andrea Burda <andrea.burda@ulb.be>
2025-03-16 13:45:22 +0100

    Fused all models into one
    To do: account for catastrophic interference

--- meta-learning_main_script.py
+++ meta-learning_main_script.py
@@ -14,35 +14,47 @@ alpha = 0.1 #learning rate
 beta = 2 #reverse temperature
 w = np.random.random(3) #weights
 
-#STATIC MODEL DATA
-train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]]) #data set
-test_x = np.copy(train_x)
+#X DATA
+##data set
+x_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+##RM context
+context_vector_RM = np.array([0, 0, 1])
+context_vector_RM = np.tile(context_vector_RM, (4, 1))
+
+train_x_RM = np.hstack([x_inputs, context_vector_RM]) 
+test_x_RM = np.copy(train_x_RM)
+
+##XOR context
+context_vector_XOR = np.array([0, 1, 0])
+context_vector_XOR = np.tile(context_vector_XOR, (4, 1))
+
+train_x_XOR = np.hstack([x_inputs, context_vector_XOR]) 
+test_x_XOR = np.copy(train_x_XOR)
+
+##AND context
+context_vector_AND = np.array([1, 0, 0])
+context_vector_AND = np.tile(context_vector_AND, (4, 1))
+
+train_x_AND = np.hstack([x_inputs, context_vector_AND]) 
+test_x_AND = np.copy(train_x_AND)
+
+#STATIC Y DATA
 train_y_AND = np.array([0, 0, 0, 1]).reshape(4, 1)
 train_y_XOR = np.array([0, 1, 1, 0]).reshape(4, 1)
 
-#MODELS STRUCTURE
-n_input, n_output = train_x.shape[1], 1 
 
-model_dict = {
-    'AND_model': Sequential([
-        Dense(4, activation='tanh', input_shape=(n_input,)),
-        Dense(n_output, activation='sigmoid')
-        ]),
-    'XOR_model': Sequential([
-        Dense(4, activation='tanh', input_shape=(n_input,)),
-        Dense(n_output, activation='sigmoid')
-        ]),
-    'RM_model': Sequential([
-        Dense(4, activation='tanh', input_shape=(n_input,)),
-        Dense(n_output, activation='sigmoid')
-        ]),
-    }
 
-for model in model_dict.values(): 
-    model.compile(
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.1), 
-        loss = tf.keras.losses.BinaryCrossentropy(),
-        metrics = [tf.keras.metrics.BinaryAccuracy()]
+#MODEL STRUCTURE
+model = Sequential([
+    Dense(4, activation='tanh', input_shape=(5,)),
+    Dense(1, activation='sigmoid')
+    ])
+
+model.compile(
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.1), 
+    loss = tf.keras.losses.BinaryCrossentropy(),
+    metrics = [tf.keras.metrics.BinaryAccuracy()]
     )
 
 #MAIN LOOP 
@@ -59,7 +71,7 @@ for i in range(n_trials):
     data.loc[i, 'trial'] = i + 1
     print("trial number:", data.loc[i, 'trial'])
      
-    #changeable data
+    #changeable y_data
     train_y_RM = np.random.randint(0, 2, size=(4, 1))
     
     #make a choice (softmax function)
@@ -71,35 +83,32 @@ for i in range(n_trials):
     data.loc[i, 'chosen_task'] = chosen_task 
     action_counts[chosen_task] += 1
     
-##running the chosen task and retrieving previous accuracy
-    if chosen_task == 2:
-        model_name = 'AND_model'
-        model = model_dict[model_name]
-        history = model.fit(train_x, train_y_AND, batch_size = 1, epochs=epochs)
-        
-    elif chosen_task == 1:
-        model_name = 'XOR_model'
-        model = model_dict[model_name]
-        history = model.fit(train_x, train_y_XOR, batch_size = 1, epochs=epochs)
-        
-    elif chosen_task == 0: 
-        model_name = 'RM_model'
-        model = model_dict[model_name]
-        history = model.fit(train_x, train_y_RM, batch_size = 1, epochs=epochs)    
+##set the context
+    if chosen_task == 0:
+        task_name = 'RM_task'
+        history = model.fit(train_x_RM, train_y_RM, batch_size = 1, epochs=epochs)
+
+    elif chosen_task == 1: 
+        task_name = 'XOR_task'
+        history = model.fit(train_x_XOR, train_y_XOR, batch_size = 1, epochs=epochs)
+              
+    elif chosen_task == 2:
+        task_name = 'AND_task'
+        history = model.fit(train_x_AND, train_y_AND, batch_size = 1, epochs=epochs)
     
 ##record data
-    test_results_AND = model_dict['AND_model'].evaluate(train_x, train_y_AND)
-    data.loc[i, 'loss_AND'] = test_results_AND[0]
-    data.loc[i, 'acc_AND'] = test_results_AND[1]
-    
-    test_results_XOR = model_dict['XOR_model'].evaluate(train_x, train_y_XOR)
-    data.loc[i, 'loss_XOR'] = test_results_XOR[0]
-    data.loc[i, 'acc_XOR'] = test_results_XOR[1]
-    
-    test_results_RM = model_dict['RM_model'].evaluate(train_x, train_y_RM)
+    test_results_RM = model.evaluate(train_x_RM, train_y_RM)
     data.loc[i, 'loss_RM'] = test_results_RM[0]
     data.loc[i, 'acc_RM'] = test_results_RM[1]
+
+    test_results_XOR = model.evaluate(train_x_XOR, train_y_XOR)
+    data.loc[i, 'loss_XOR'] = test_results_XOR[0]
+    data.loc[i, 'acc_XOR'] = test_results_XOR[1]
         
+    test_results_AND = model.evaluate(train_x_AND, train_y_AND)
+    data.loc[i, 'loss_AND'] = test_results_AND[0]
+    data.loc[i, 'acc_AND'] = test_results_AND[1]
+          
 ##averaging previous accuracies
     trial_accuracy = history.history["binary_accuracy"][0]
     accuracy_history[chosen_task].append(trial_accuracy)
@@ -115,37 +124,27 @@ for i in range(n_trials):
     data.loc[i, 'reward'] = reward
     w[chosen_task] += alpha*(reward - w[chosen_task]) #Rescorla-Wagner learning rule
     print(data['reward'].iloc[-1])   
-    
-##record accuracy for the chosen task
-    if chosen_task == 2: 
-        previous_AND_accuracy = trial_accuracy
-        
-    elif chosen_task == 1: 
-        previous_XOR_accuracy = trial_accuracy
-      
-    elif chosen_task == 0:
-        previous_RM_accuracy = trial_accuracy
 
 
 ##GATHER DATA
 ##test the models
-test_results_AND = model_dict['AND_model'].evaluate(test_x, train_y_AND)
-test_predictions_AND = model_dict['AND_model'].predict(test_x)
+test_results_AND = model.evaluate(test_x_AND, train_y_AND)
+test_predictions_AND = model.predict(test_x_AND)
 
-test_results_XOR = model_dict['XOR_model'].evaluate(test_x, train_y_XOR)
-test_predictions_XOR = model_dict['XOR_model'].predict(test_x)
+test_results_XOR = model.evaluate(test_x_XOR, train_y_XOR)
+test_predictions_XOR = model.predict(test_x_XOR)
 
-test_results_RM = model_dict['RM_model'].evaluate(test_x, train_y_RM)
-test_predictions_RM = model_dict['RM_model'].predict(test_x)
+test_results_RM = model.evaluate(test_x_RM, train_y_RM)
+test_predictions_RM = model.predict(test_x_RM)
 
 ##summary
 print(["Weights:", w])
 print("Action selection frequency:", action_counts)
 print("Most selected action:", max(action_counts, key=action_counts.get))
 print("mean reward: {:.2f}".format(np.mean(data['reward'])))
-print(f"Model name: AND Model, Loss: {test_results_AND[0]:.4f}, Accuracy: {test_results_AND[1]:.4f}")
-print(f"Model name: XOR Model, Loss: {test_results_XOR[0]:.4f}, Accuracy: {test_results_XOR[1]:.4f}")
-print(f"Model name: RM Model, Loss: {test_results_RM[0]:.4f}, Accuracy: {test_results_RM[1]:.4f}")
+print(f"Task name: AND, Loss: {test_results_AND[0]:.4f}, Accuracy: {test_results_AND[1]:.4f}")
+print(f"Task name: XOR, Loss: {test_results_XOR[0]:.4f}, Accuracy: {test_results_XOR[1]:.4f}")
+print(f"Task name: RM, Loss: {test_results_RM[0]:.4f}, Accuracy: {test_results_RM[1]:.4f}")
 
 ##plots
 window_conv = 5
@@ -173,7 +172,7 @@ avg_RM_acc = np.convolve(data['acc_RM'], np.ones(window_conv)/window_conv, mode 
 avg_XOR_acc = np.convolve(data['acc_XOR'], np.ones(window_conv)/window_conv, mode = 'valid')
 avg_AND_acc = np.convolve(data['acc_AND'], np.ones(window_conv)/window_conv, mode = 'valid')
 avg_mean_acc = np.convolve(data['mean_acc'], np.ones(window_conv)/window_conv, mode = 'valid')
-ax.set_title("Comparison of Accuracy over Time for Each Model")
+ax.set_title("Comparison of Accuracy over Time for Each Task")
 ax.plot(avg_AND_acc, color = "blue", label = "AND")
 ax.plot(avg_XOR_acc, color = "green", label = "XOR")
 ax.plot(avg_RM_acc, color = "red", label = "RM")
@@ -188,7 +187,7 @@ plt.close(fig)
 data['mean_loss'] = data.loc[:, ['loss_AND', 'loss_XOR', 'loss_RM']].mean(axis=1)
  
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.set_title("Comparison of Loss Functions for Each Model")
+ax.set_title("Comparison of Loss Functions for Each Task")
 avg_RM_loss = np.convolve(data['loss_RM'], np.ones(window_conv)/window_conv, mode = 'valid')
 avg_XOR_loss = np.convolve(data['loss_XOR'], np.ones(window_conv)/window_conv, mode = 'valid')
 avg_AND_loss = np.convolve(data['loss_AND'], np.ones(window_conv)/window_conv, mode = 'valid')
